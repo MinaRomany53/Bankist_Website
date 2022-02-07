15182957e22bfe1dffdf7f646ae1b9ac4b1bcc43
MinaRomany53 <minaromany190@gmail.com>
2022-02-08 00:10:45 +0200

    new

---
+++ js/lectures.js
@@ -0,0 +1,263 @@
+"use strict";
+
+// DOM API METHODS Referrance
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+
+// Selecting Elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+//////////////Uisng This Methods All the Time///////////////////
+const headerEL = document.querySelector(".header");
+console.log(headerEL);
+const sectionsList = document.querySelectorAll(".section");
+console.log(sectionsList); // Returns a Node List
+///////////////////////////////////////
+
+const sectionEL = document.getElementById("section--1");
+console.log(sectionEL);
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons); // Returns a Html Collection -- Live collection update if any DOM change occur
+const btns = document.getElementsByClassName("btn");
+console.log(btns);
+
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+
+// Creating and Inserting Elements
+//.insertAdjacentHTML (easy method)
+/* 
+containerMovements.insertAdjacentHTML("afterbegin", rowHtml); from Dispaly Movments Function at Bankist Project
+rowHtml => Is a String variable stores Html code
+*/
+
+// or creating element from scratch
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.innerHTML =
+  'We use cookies for improved functionality and analytics.  <button class="btn btn__cookie--close">Got it!</button>'; // or use (.textContent)
+console.log(message);
+const headerElement = document.querySelector(".header");
+// headerElement.append(message); // insert this new elemetn at bottom
+// headerElement.prepend(message);
+// headerElement.after(message);
+// headerElement.before(message);
+
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+
+// Deleting Elements
+/*
+const btnCloseCookie = document.querySelector(".btn__cookie--close");
+btnCloseCookie.addEventListener("click", () => {
+  // message.remove();
+  message.classList.add("hidden"); // not deleted the element but make nice transition
+});
+*/
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+
+// Styles
+message.style.backgroundColor = "#37383d";
+message.style.setProperty("background-color", "#37383d"); //or this method
+
+console.log(message.style.backgroundColor);
+console.log(message.style.height); // only read Inline Styles
+console.log(message.style.color); // only read Inline Styles
+
+console.log(getComputedStyle(message)); // get all Styles
+console.log(getComputedStyle(message).height);
+console.log(getComputedStyle(message).color);
+
+// Change :root Variables styles (at documentElement)
+// document.documentElement.style.setProperty("--color-primary", "darkorange");
+
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+
+//Attributes
+const logo = document.querySelector(".logo");
+console.log(logo.getAttribute("src"));
+console.log(logo.getAttribute("alt"));
+
+logo.setAttribute("alt", "Bankist Awesome Logo 2022");
+console.log(logo.getAttribute("alt"));
+
+// data attributes
+console.log(logo.dataset.versionNumber);
+logo.dataset.versionNumber = "4.5";
+console.log(logo.dataset.versionNumber);
+
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+
+// Clases
+// logo.classList.add();
+// logo.classList.remove();
+// logo.classList.toggle();
+// logo.classList.contains();
+
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+
+// Events and Event Handlers
+// https://developer.mozilla.org/en-US/docs/Web/Events
+const h1 = document.querySelector(".landing__title h1");
+const alertH1 = (e) => {
+  alert(`Great! you are reading the heading :D`);
+
+  h1.removeEventListener("mouseenter", alertH1); // remove this event after occur once.
+};
+
+// h1.addEventListener("mouseenter", alertH1); // add event when hover on h1
+
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+
+// Event Propagationانتشار او توالد   Bubbling and Capturing
+const randomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const randomColor = function () {
+  return `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+};
+
+/*
+const navLink = document.querySelector(".nav__link");
+navLink.addEventListener("mouseenter", (e) => {
+  navLink.style.backgroundColor = randomColor();
+
+  // to stop Propagation
+  // e.stopPropagation();
+});
+
+const navLinks = document.querySelector(".nav__links");
+navLinks.addEventListener("mouseenter", () => {
+  navLinks.style.backgroundColor = randomColor();
+});
+
+const nav = document.querySelector(".nav");
+nav.addEventListener("mouseenter", () => {
+  nav.style.backgroundColor = randomColor();
+});
+*/
+console.log("-------------------------");
+console.log("-------------------------");
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+
+// DOM Traversing
+const H1 = document.querySelector("h1");
+
+// going downwards: Childs
+console.log(H1.querySelectorAll(".highlight"));
+console.log(H1.childNodes); // all nodes in this element
+console.log(H1.children); // best
+console.log(H1.firstElementChild);
+console.log(H1.lastElementChild);
+// H1.firstElementChild.style.color = "blue";
+// H1.lastElementChild.style.color = "orange";
+
+// going upwards: Parents
+console.log(H1.parentNode);
+console.log(H1.parentElement);
+
+H1.closest(".header"); // ooposite of querySelector (select parents)
+// H1.closest(".header").style.background = "var( --gradient-secondary)";
+
+// going Sideways: Siblings
+console.log(h1.previousElementSibling); // no sibilings before it
+console.log(h1.nextElementSibling);
+
+console.log(h1.parentElement.children); // nice trick to get all sibilings as HTML Collections
+
+console.log([...h1.parentElement.children]); // convert it to an Array
+
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+
+// Implemetn Sticky Navigation using The Intersection Observer رصدAPI
+/*
+const section_1 = document.querySelector("#section--1");
+
+const obsCallback = function (entries, observer) {
+  entries.forEach((entry) => {
+    console.log(entry);
+  });
+};
+const obsOptions = {
+  root: null,
+  threshold: 0.1,
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+
+observer.observe(section_1);
+*/
+
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
+///////////////////////////////////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////
+//////////////////////////////////////
